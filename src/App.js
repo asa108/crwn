b7b176b8a51949647eb836b44e823b6a5bde8c7a
Asane <asane@AsanenoMacBook-Air.local>
2020-07-02 21:50:13 +0900

    updating pou app with ashop page ,collection-preview,collection-item

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
+//Switchで最初に該当したURLがレンダーされたら後からのコードは読まれない
 
 export default App;
